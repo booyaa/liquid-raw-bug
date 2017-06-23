a06eab2ab54632b414f03077894334fc71777da4
booyaa <booyaabooyaabooyaa@gmail.com>
2017-06-23 08:34:14 +0100

    fix: remove unrelated code, clean up example

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 extern crate liquid;
 
 fn main() {
-use liquid::{Renderable, Context, Value};
+    use liquid::Renderable;
 
-let template = liquid::parse(
-r"{% raw %}
+    let template = liquid::parse(r"{% raw %}
 {{ abc }}
 {{ def }}
 {{ ghi }}
-{% endraw %}", Default::default()).unwrap();
+{% endraw %}",
+                                 Default::default())
+        .unwrap();
 
-let mut context = Context::new();
-context.set_val("num", Value::Num(4f32));
-
-let output = template.render(&mut context);
-assert_eq!(output.unwrap(), Some("\n{{ abc }}\n{{ def }}\n{{ ghi }}\n".to_string()));
+    let output = template.render(&mut Default::default());
+    assert_eq!(output.unwrap(),
+               Some("\n{{ abc }}\n{{ def }}\n{{ ghi }}\n".to_string()));
 }
